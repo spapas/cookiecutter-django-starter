a980957b577650d20e2443adca534670ea4b8947
Serafeim Papastefanos <spapas@gmail.com>
2016-07-14 14:18:27 +0300

    Add a global perm holder

--- {{cookiecutter.project_name}}/{{cookiecutter.project_name}}/core/models.py
+++ {{cookiecutter.project_name}}/{{cookiecutter.project_name}}/core/models.py
@@ -1,5 +1,11 @@
 from __future__ import unicode_literals
-
 from django.db import models
 
-# Create your models here.
+class GlobalPermissionHolder(models.Model):
+    "A non-managed model to be used as a holder for global permissions"
+    class Meta:
+        managed = False  # No database table creation or deletion operations will be performed for this model.
+        permissions = (
+            ('user', 'Application user'),
+            ('admin', 'Application admin'),
+        )
