134e0510dac8e468672c117b311b345930573daf
Serafeim Papastefanos <spapas@gmail.com>
2017-09-07 10:44:02 +0300

    Compress offline = true

--- {{cookiecutter.project_name}}/{{cookiecutter.project_name}}/settings/prod.py
+++ {{cookiecutter.project_name}}/{{cookiecutter.project_name}}/settings/prod.py
@@ -3,6 +3,8 @@ from .base import *
 # These should be imprted from from base.py but I'll redefine them for clarity
 DEBUG = False
 SITE_ID = 1
+COMPRESS_OFFLINE = True
+
 
 INSTALLED_APPS += (
     'raven.contrib.django.raven_compat',
