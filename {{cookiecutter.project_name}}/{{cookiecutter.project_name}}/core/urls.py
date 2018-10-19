45028cd63a613cc5078271cb69c6f8a4e943f21f
Serafeim Papastefanos <spapas@gmail.com>
2018-10-19 11:12:52 +0300

    Use Login/LogoutView

--- {{cookiecutter.project_name}}/{{cookiecutter.project_name}}/core/urls.py
+++ {{cookiecutter.project_name}}/{{cookiecutter.project_name}}/core/urls.py
@@ -1,9 +1,9 @@
 from django.conf.urls import url
-from django.contrib.auth.views import login, logout
+from django.contrib.auth.views import LoginView, LogoutView
 from django.views.generic import TemplateView
 
 urlpatterns = [
     url(r"^$", TemplateView.as_view(template_name="home.html"), name="home"),
-    url(r"^login/$", login, name="auth_login"),
-    url(r"^logout/$", logout, {"template_name": "logout.html"}, name="auth_logout"),
+    url(r"^login/$", LoginView.as_view(), name="auth_login"),
+    url(r"^logout/$", LogoutView.as_view(), {"template_name": "logout.html"}, name="auth_logout"),
 ]
