4b5d889a91b42d420fb8495a8ba595372e631bcf
Serafeim Papastefanos <spapas@gmail.com>
2018-11-20 13:53:22 +0200

    Update urls.py

--- {{cookiecutter.project_name}}/{{cookiecutter.project_name}}/urls.py
+++ {{cookiecutter.project_name}}/{{cookiecutter.project_name}}/urls.py
@@ -2,17 +2,19 @@
 {{cookiecutter.project_name}} URL Configuration
 """
 from django.conf import settings
-from django.conf.urls import url, include
+from django.urls import path, include
 from django.conf.urls.static import static
 from django.contrib import admin
 
 urlpatterns = [
-    url(r"^", include("{{cookiecutter.project_name}}.core.urls")),
-    url(r"^admin/", admin.site.urls),
+    path("", include("ebpm.core.urls")),
+    path("process/", include("ebpm.process.urls")),
+    path("admin/", admin.site.urls),
 ]
 
 if settings.DEBUG:
     import debug_toolbar
 
-    urlpatterns = [url(r"^__debug__/", include(debug_toolbar.urls))] + urlpatterns
+    urlpatterns = [path("__debug__/", include(debug_toolbar.urls))] + urlpatterns
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
