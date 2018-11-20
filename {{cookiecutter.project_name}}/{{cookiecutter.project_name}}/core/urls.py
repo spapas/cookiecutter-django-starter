5b6c270c46f01f28f33781b98b74d5e8de23cd28
Serafeim Papastefanos <spapas@gmail.com>
2018-11-20 13:53:57 +0200

    Update urls.py

--- {{cookiecutter.project_name}}/{{cookiecutter.project_name}}/core/urls.py
+++ {{cookiecutter.project_name}}/{{cookiecutter.project_name}}/core/urls.py
@@ -1,9 +1,9 @@
-from django.conf.urls import url
+from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    url(r"^$", TemplateView.as_view(template_name="home.html"), name="home"),
-    url(r"^login/$", LoginView.as_view(), name="auth_login"),
-    url(r"^logout/$", LogoutView.as_view(), {"template_name": "logout.html"}, name="auth_logout"),
+    path(r"", TemplateView.as_view(template_name="home.html"), name="home"),
+    path(r"login/", LoginView.as_view(), name="auth_login"),
+    path(r"logout/", LogoutView.as_view(), {"template_name": "logout.html"}, name="auth_logout"),
 ]
