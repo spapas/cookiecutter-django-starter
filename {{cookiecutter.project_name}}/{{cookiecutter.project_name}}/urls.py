b7ba1052c960635c3182867dc0cfbd5956fc05f0
Serafeim Papastefanos <spapas@gmail.com>
2020-08-06 22:44:28 +0300

    A small fix

--- {{cookiecutter.project_name}}/{{cookiecutter.project_name}}/urls.py
+++ {{cookiecutter.project_name}}/{{cookiecutter.project_name}}/urls.py
@@ -13,8 +13,11 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
-    import debug_toolbar
+    try:
+        import debug_toolbar
 
-    urlpatterns = [path("__debug__/", include(debug_toolbar.urls))] + urlpatterns
+        urlpatterns = [path("__debug__/", include(debug_toolbar.urls))] + urlpatterns
+    except:
+        pass
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
