2a6d1b15d13b61e72dff98fcf3a9bdbe97a45c8a
Serafeim Papastefanos <spapas@gmail.com>
2021-10-06 13:13:36 +0300

    Update base.py

--- {{cookiecutter.project_name}}/{{cookiecutter.project_name}}/settings/base.py
+++ {{cookiecutter.project_name}}/{{cookiecutter.project_name}}/settings/base.py
@@ -45,6 +45,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "django_tools.middlewares.ThreadLocal.ThreadLocalMiddleware",
 ]
 
 ROOT_URLCONF = "{{cookiecutter.project_name}}.urls"
