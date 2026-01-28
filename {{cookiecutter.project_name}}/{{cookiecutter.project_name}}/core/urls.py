900c998b85e1533cd30d9856ce8878298bd02666
Serafeim Papastefanos <spapas@gmail.com>
2026-01-28 12:37:30 +0200

    Update urls.py

--- {{cookiecutter.project_name}}/{{cookiecutter.project_name}}/core/urls.py
+++ {{cookiecutter.project_name}}/{{cookiecutter.project_name}}/core/urls.py
@@ -1,13 +1,22 @@
 from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
 from django.views.generic import TemplateView
-from .views import HijackFormView
-from django.contrib.auth.decorators import user_passes_test
+from django.contrib.auth.decorators import permission_required
+from .views import hijack_form, hijack_action
 
 
 urlpatterns = [
     path("", TemplateView.as_view(template_name="home.html"), name="home"),
     path("login/", LoginView.as_view(), name="auth_login"),
     path("logout/", LogoutView.as_view(template_name="logout.html"), name="auth_logout"),
-    path("hijack-action/", user_passes_test(lambda u: u.is_superuser is True)(HijackFormView.as_view()), name="hijack-action"),
+        path(
+        "hijack-form/",
+        permission_required("hijack.permissions.superusers_only")(hijack_form),
+        name="hijack-form",
+    ),
+    path(
+        "hijack-action/",
+        permission_required("hijack.permissions.superusers_only")(hijack_action),
+        name="hijack-action",
+    ),
 ]
